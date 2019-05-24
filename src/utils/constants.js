32cf0267eb804b9e25a6f0e6ac6e0fb2c1c44362
Kevin Jennison <kevin.jennison1@gmail.com>
2019-05-24 12:13:52 -0700

    Use "search" key in local storage

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -5,6 +5,6 @@ export const UNSUPPORTED_BROWSER = 'other'
 
 // localStorage keys
 export const STORAGE_REFERRAL_DATA_REFERRING_USER =
-  'tab.referralData.referringUser'
+  'search.referralData.referringUser'
 export const STORAGE_REFERRAL_DATA_REFERRING_CHANNEL =
-  'tab.referralData.referringChannel'
+  'search.referralData.referringChannel'
